e47e664cef638f24998b48e6b3b37a070c22c982
kubok758 <artemborovitskiy0312@gmail.com>
2025-04-04 19:12:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.getElementById('downloadButton').addEventListener('click', function() {
     // Путь к файлу, который нужно скачать
-    const fileUrl = 'https://github.com/kubok758/asmortfoncheg/raw/main/asmortfoncheg.vbs'; // Укажите путь к файлу
-
+    const fileUrl = 'https://github.com/kubok758/asmortfoncheg/raw/main/asmortfoncheg.vbs';  // Путь к файлу на GitHub
+    
     // Создаём временную ссылку для скачивания
     const link = document.createElement('a');
     link.href = fileUrl;
-    link.download = 'asmartfoncheg.zip';  // Имя файла при скачивании
-
+    link.download = 'asmortfoncheg.vbs';  // Имя файла при скачивании
+    
     // Инициируем скачивание
     link.click();
 });
